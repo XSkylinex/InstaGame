693ca20102cb8a19358405c3a4261002f837458e
Alex <alex1mosh@gmail.com>
2020-03-22 01:52:47 +0200

    add icons and nav bar

---
+++ app/src/main/java/com/example/test/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.example.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.test.ui.login.LoginFragment;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, LoginFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/test/ui/login/LoginFragment.java
@@ -0,0 +1,39 @@
+package com.example.test.ui.login;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.test.R;
+
+public class LoginFragment extends Fragment {
+
+    private LoginViewModel mViewModel;
+
+    public static LoginFragment newInstance() {
+        return new LoginFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.login_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(LoginViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
