4ee3fdbbfecf3385f1ba3cac311ea306abf1da43
Alex <alex1mosh@gmail.com>
2020-03-22 03:08:35 +0200

    working login

--- app/src/main/java/com/example/test/ui/login/LoginFragment.java
+++ app/src/main/java/com/example/test/ui/login/LoginFragment.java
@@ -4,6 +4,7 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProviders;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -14,14 +15,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.test.MainActivity;
 import com.example.test.R;
+import com.example.test.contollers.Auth;
+import com.example.test.contollers.database.Database;
+import com.example.test.models.User;
 import com.example.test.ui.register.RegisterFragment;
+
+import java.util.function.Consumer;
 
 public class LoginFragment extends Fragment {
 
-    private LoginViewModel mViewModel;
+//    private LoginViewModel mViewModel;
     private Button btnToRegister;
+    private Button btnSignIn;
+    private EditText et_email, et_password;
 
     public static LoginFragment newInstance() {
         return new LoginFragment();
@@ -37,13 +48,16 @@ public class LoginFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         btnToRegister = (Button) view.findViewById(R.id.btn_login_to_register);
+        btnSignIn = (Button) view.findViewById(R.id.btn_signin);
+        et_email = (EditText) view.findViewById(R.id.et_email);
+        et_password = (EditText) view.findViewById(R.id.et_password);
         super.onViewCreated(view, savedInstanceState);
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        mViewModel = ViewModelProviders.of(this).get(LoginViewModel.class);
+//        mViewModel = ViewModelProviders.of(this).get(LoginViewModel.class);
 
         btnToRegister.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,6 +67,39 @@ public class LoginFragment extends Fragment {
                 fragmentTransaction.replace(R.id.login_container, RegisterFragment.newInstance());
                 fragmentTransaction.addToBackStack(null);
                 fragmentTransaction.commit();
+            }
+        });
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = et_email.getText()+"";
+                String password = et_password.getText()+"";
+
+                Auth.SignIn(email, password, new Consumer<String>() {
+                    @Override
+                    public void accept(String userId) {
+                        Database.User.getUser(userId, new Consumer<User>() {
+                            @Override
+                            public void accept(User user) {
+                                Toast.makeText(getContext(), "Welcome back "+user.get_userName()+"!", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getActivity(), MainActivity.class);
+                                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                startActivity(intent);
+                            }
+                        }, new Consumer<Exception>() {
+                            @Override
+                            public void accept(Exception e) {
+                                Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                }, new Consumer<Exception>() {
+                    @Override
+                    public void accept(Exception e) {
+                        Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
 
